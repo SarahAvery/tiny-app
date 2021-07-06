9de6f1bda041b69c8f797c14aaa62f33c8ca3103
S Avery <sarahashavery@gmail.com>
2021-07-06 16:20:43 +0000

    delete

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+//
+//............Random string for new URLs
 function generateRandomString() {
   const randomChars =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -10,12 +12,20 @@ function generateRandomString() {
   return result;
 }
 
-// console.log(generateRandomString());
-
+//
+//............SETUP............//
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+//
+//............Middleware
 const bodyParser = require("body-parser");
 app.use(
   bodyParser.urlencoded({
@@ -25,23 +35,22 @@ app.use(
 
 // app.use(express.urlencoded());
 
-app.set("view engine", "ejs");
+//
+//............Style
+// app.use(express.static("public"));
 
-//............Database
+//
+//............DATABASE............//
 let urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//--------------------------------//
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-//Routes
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -50,18 +59,17 @@ app.get("/shortURL.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//
 //............TEMPLATES............//
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-//............
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-//............
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -70,26 +78,23 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-//............
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+//--------------------------------//
 
-//............
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+//
 //............Generate randome string, add to database
 app.post("/urls", (req, res) => {
-  // console.log(req.body.longURL);
   const shortURL = generateRandomString();
   longURL = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
   urlDatabase[shortURL] = `${longURL}`;
 });
 
+//
 //............Delete URL
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -97,10 +102,9 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+//
 //............Edit and Update URL
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
-
-app.use(express.static("public"));
